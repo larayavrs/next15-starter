b425a1c62dc22760ffb07d326abb9fe59e8641b4
Leo Araya <larayavrs@gmail.com>
2024-12-22 17:58:41 -0300

    feat: including vitest as a official project testing performance
    testing changes and new offers

---
+++ example.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest';
+
+// Test situation example
+describe('example', () => {
+  test('given a passing test: passes', () => {
+    expect(1).toStrictEqual(1);
+  });
+});
